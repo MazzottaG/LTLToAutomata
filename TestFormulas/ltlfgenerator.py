fd586c76a62414d96aca0a48cad4c6cfd414ca4b
Rafael Peñaloza <rafael.penaloza@unimib.it>
2023-05-19 16:24:19 +0200

    generatore aggiornato

--- TestFormulas/ltlfgenerator.py
+++ TestFormulas/ltlfgenerator.py
@@ -8,15 +8,24 @@
 #####################
 
 ## produce the encoding of formula RespondedExistence(n)
-def RE (n,file):
+def RE (n,file,long=0):
 	count = 7
 	file.write('neg(1,p0).\nor(2,p0,1).\nuntil(3,2,p0).\nnext(4,3).\nneg(5,3).\nor(6,p1,p2).\n')	
 	for i in range(3,n+1):
 		file.write('or(' + str(count) + ',' + str(count-1) + ',p' + str(i) + ').\n')
 		count += 1
 	file.write('until(' + str(count) + ',2,' + str(count-1) + ').\nnext(' + str(count+1) + ',' + str(count) + ').\n')
-	file.write('or(' + str(count+2) + ',5,' + str(count) + ').\nisPhi(' + str(count+2) + ').\n')
+	file.write('or(' + str(count+2) + ',5,' + str(count) + ').\n')
 	count += 3
+	#### long models
+	for i in range(long):
+		if i == 0:
+			file.write('next(' + str(count) + ',1).\n')
+		else:
+			file.write('next(' + str(count) + ',' + str(count-1) + ').\n')
+		count += 1
+	#### end long models
+	file.write('isPhi(' + str(count-1) + ').\n')
 	formula = count
 	for i in range(1,n+1):
 		file.write('neg(' + str(count) + ',p' + str(i) + ').\n')
@@ -34,7 +43,7 @@ def RE (n,file):
 
 
 ## produce the encoding of formula Response(n)
-def R (n,file):
+def R (n,file,long=0):
 	count = 4
 	file.write('neg(1,p0).\nor(2,p0,1).\nor(6,p1,p2).\n')	
 	for i in range(3,n+1):
@@ -44,8 +53,17 @@ def R (n,file):
 	file.write('and(' + str(count+2) + ',p0,' + str(count) + ').\n')  
 	count += 2
 	file.write('until(' + str(count) + ',2,' + str(count-1) + ').\nnext(' + str(count+1) + ',' + str(count) + ').\n')
-	file.write('neg(' + str(count+2) + ',' + str(count) + ').\nisPhi(' + str(count+2) + ').\n')
+	file.write('neg(' + str(count+2) + ',' + str(count) + ').\n')
 	count += 3
+	#### long models
+	for i in range(long):
+		if i == 0:
+			file.write('next(' + str(count) + ',1).\n')
+		else:
+			file.write('next(' + str(count) + ',' + str(count-1) + ').\n')
+		count += 1
+	#### end long models
+	file.write('isPhi(' + str(count-1) + ').\n')
 	formula = count
 	for i in range(1,n+1):
 		file.write('neg(' + str(count) + ',p' + str(i) + ').\n')
@@ -61,7 +79,7 @@ def R (n,file):
 
 
 ## produce the encoding of formula AlternateResponse(n)
-def AR (n,file):
+def AR (n,file,long=0):
 	count = 4
 	file.write('neg(1,p0).\nor(2,p0,1).\nor(6,p1,p2).\n')	
 	for i in range(3,n+1):
@@ -71,8 +89,17 @@ def AR (n,file):
 	file.write('or(' + str(count+2) + ',1,' + str(count+1) + ').\n')  
 	count += 3
 	file.write('until(' + str(count) + ',2,' + str(count-1) + ').\nnext(' + str(count+1) + ',' + str(count) + ').\n')
-	file.write('neg(' + str(count+2) + ',' + str(count) + ').\nisPhi(' + str(count+2) + ').\n')
+	file.write('neg(' + str(count+2) + ',' + str(count) + ').\n')
 	count += 3
+	#### long models
+	for i in range(long):
+		if i == 0:
+			file.write('next(' + str(count) + ',1).\n')
+		else:
+			file.write('next(' + str(count) + ',' + str(count-1) + ').\n')
+		count += 1
+	#### end long models
+	file.write('isPhi(' + str(count-1) + ').\n')
 	formula = count
 	for i in range(1,n+1):
 		file.write('neg(' + str(count) + ',p' + str(i) + ').\n')
@@ -98,8 +125,17 @@ def CR (n,file,long=0):
 	file.write('or(' + str(count+1) + ',1,' + str(count) + ').\n')  
 	count += 2
 	file.write('until(' + str(count) + ',2,' + str(count-1) + ').\nnext(' + str(count+1) + ',' + str(count) + ').\n')
-	file.write('neg(' + str(count+2) + ',' + str(count) + ').\nisPhi(' + str(count+2) + ').\n')
+	file.write('neg(' + str(count+2) + ',' + str(count) + ').\n')
 	count += 3
+	#### long models
+	for i in range(long):
+		if i == 0:
+			file.write('next(' + str(count) + ',1).\n')
+		else:
+			file.write('next(' + str(count) + ',' + str(count-1) + ').\n')
+		count += 1
+	#### end long models
+	file.write('isPhi(' + str(count-1) + ').\n')
 	formula = count
 	for i in range(1,n+1):
 		file.write('neg(' + str(count) + ',p' + str(i) + ').\n')
